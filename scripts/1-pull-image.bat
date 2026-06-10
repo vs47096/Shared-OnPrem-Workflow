23bca65ff217344c34c08d4fb88b54a196e87760
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2026-06-10 12:09:50 +0530

    Add script to pull and tag Docker images

---
+++ scripts/1-pull-image.bat
@@ -0,0 +1,21 @@
+@echo off
+set REMOTE_IMAGE=%1
+set TAG_NAME=%2
+
+echo =======================================================
+echo [Step 1/4] Pulling and Tagging Image from Docker Hub
+echo =======================================================
+echo Target Image: %REMOTE_IMAGE%
+echo Local Tag:    %TAG_NAME%
+
+docker pull %REMOTE_IMAGE%
+if %ERRORLEVEL% NEQ 0 (
+    echo [ERROR] Failed to pull image from Docker Hub.
+    exit /b 1
+)
+
+docker tag %REMOTE_IMAGE% %TAG_NAME%
+docker rmi %REMOTE_IMAGE%
+echo y | docker image prune
+
+echo [SUCCESS] Step 1 Complete.
